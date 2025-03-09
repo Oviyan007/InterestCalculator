729cdc89997fc4cda9791809e6fad5cb1a0a8387
Oviyan S <86316218+Oviyan007@users.noreply.github.com>
2025-03-09 12:26:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ document.getElementById("calculate").addEventListener("click", function () {
 document.getElementById("setRemainder").addEventListener("click", function () {
     const borrower = document.getElementById("borrower").value.trim();
     const dueDate = new Date(document.getElementById("dueDate").value);
-    dueDate.setHours(11, 10, 0); // Example: Notification at 9 AM
+    dueDate.setHours(8, 0, 0); // Example: Notification at 9 AM
 
     if (!borrower) {
         alert("Please enter the borrower's name.");
